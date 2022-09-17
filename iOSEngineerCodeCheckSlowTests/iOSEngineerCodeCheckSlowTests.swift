71db437149c3b992ff923a1bfb5252acdb72d0f5
0Itsuki0 <2018.itsuki@gmail.com>
2022-09-17 16:11:03 +0900

    add unit test for Repository Data Manager function: fetchAvatarImage

--- iOSEngineerCodeCheckSlowTests/iOSEngineerCodeCheckSlowTests.swift
+++ iOSEngineerCodeCheckSlowTests/iOSEngineerCodeCheckSlowTests.swift
@@ -13,6 +13,7 @@ import XCTest
 final class iOSEngineerCodeCheckSlowTests: XCTestCase {
     var repositoryDataManager: RepositoryDataManager!
     var repoDataList: [RepositoryModel]?
+    var imgData: Data?
     var expectation: XCTestExpectation?
 
     override func setUpWithError() throws {
@@ -21,12 +22,14 @@ final class iOSEngineerCodeCheckSlowTests: XCTestCase {
         repositoryDataManager = RepositoryDataManager()
         repositoryDataManager.delegate = self
         repoDataList = nil
+        imgData = nil
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         repositoryDataManager = nil
         repoDataList = nil
+        imgData = nil
         try super.tearDownWithError()
     }
 
@@ -49,23 +52,45 @@ final class iOSEngineerCodeCheckSlowTests: XCTestCase {
         // Act
         expectation = expectation(description: "fetch repository data")
         repositoryDataManager.fetchRepoData("test")
-        
+
         // Assert
         waitForExpectations(timeout: 10)
-        
         XCTAssertNotNil(repoDataList, "success in fetching repository Data")
     }
+    
+    func test_RepositoryDataMangager_fetchImgData() {
+        // Act
+        expectation = expectation(description: "fetch Image data")
+        repositoryDataManager.fetchAvatarImage(from: "https://avatars.githubusercontent.com/u/11684617?v=4")
 
+        // Assert
+        waitForExpectations(timeout: 10)
+        XCTAssertNotNil(imgData, "success in fetching Image Data")
+    }
+    
+    
+    
 }
 
 extension iOSEngineerCodeCheckSlowTests: RepositoryDataDelegate {
     func carryRepoData(_ repositoryDataManager: RepositoryDataManager, didFetchRepoData repoData: [RepositoryModel]) {
         self.repoDataList = repoData
-        expectation?.fulfill()
-        expectation = nil
+        self.expectation?.fulfill()
+        self.expectation = nil
     }
 
     func carryError(_ repositoryDataManager: RepositoryDataManager, didFailWithError error: String) {
         self.repoDataList = nil
+        self.imgData = nil
+        self.expectation = nil
     }
+    
+    func carryImgData(_ repositoryDataManager: RepositoryDataManager, didFetchImageData imgData: Data) {
+        self.imgData = imgData
+        self.expectation?.fulfill()
+        self.expectation = nil
+        
+    }
+
+    
 }
