d4b0ce96ade46d0a00c5dcb28384244907930f82
0Itsuki0 <2018.itsuki@gmail.com>
2022-09-17 17:30:10 +0900

    add UI test for search field: entering text

--- iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
+++ iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
@@ -9,12 +9,16 @@
 import XCTest
 
 class iOSEngineerCodeCheckUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
+        try super.setUpWithError()
         continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -40,4 +44,19 @@ class iOSEngineerCodeCheckUITests: XCTestCase {
             }
         }
     }
+    
+    
+    func test_UISearchBar () {
+        let searchBarElement = app.tables.searchFields["GitHubのリポジトリを検索"]
+        XCTAssertTrue(searchBarElement.exists, "search bar does not exists")
+        searchBarElement.tap()
+        searchBarElement.typeText("test")
+        
+
+    }
+    
+    
+    
+    
+    
 }
