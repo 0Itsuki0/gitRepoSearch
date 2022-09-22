d181a95b558a34acd0e3504043c65352cce0971d
0Itsuki0 <2018.itsuki@gmail.com>
2022-09-22 09:52:50 +0900

    add launch screen for playing gif

---
+++ iOSEngineerCodeCheck/Controller/LaunchViewController.swift
@@ -0,0 +1,63 @@
+//
+//  LaunchViewController.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by イツキ on 2022/09/22.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+import SwiftyGif
+
+class LaunchViewController: UIViewController {
+    
+    @IBOutlet weak var LuanchImageView: UIImageView!
+    
+    var animationView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        do {
+            let gif = try UIImage(gifName: "launchScreen.gif")
+            animationView = UIImageView(gifImage: gif, loopCount: 1)
+            animationView.frame = LuanchImageView.bounds
+            LuanchImageView.addSubview(animationView)
+            animationView.delegate = self
+            
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if animationView != nil {
+            animationView.startAnimatingGif()
+        } else {
+            performSegue(withIdentifier: "LaunchToRoot", sender: self)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+extension LaunchViewController: SwiftyGifDelegate {
+    
+    func gifDidStop(sender: UIImageView) {
+        animationView.isHidden = true
+        performSegue(withIdentifier: "LaunchToRoot", sender: self)
+    }
+}
